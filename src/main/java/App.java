098c4670d5b6084955669bdcf58b676ca0c2bbcc
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-01-21 13:57:50 +0100

    :construction: change demo code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import client.InfinimumDBClient;
+import de.hhu.bsinfo.infinileap.binding.ControlException;
 
 public final class App {
-    public static void main(String... args) {
-        InfinimumDBClient client = new InfinimumDBClient("localhost", 1234, "localhost", 4321);
+    public static void main(String... args) throws ControlException {
+        InfinimumDBClient client = new InfinimumDBClient("localhost", 2998, "localhost", 4321);
+        client.put("Hello World", "Hello World");
     }
 }
