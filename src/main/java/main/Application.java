67e12e0a452ec4e5e5a94a749f05e7a63113b483
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-02-10 15:27:08 +0100

    :recycle: change up demo code

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,10 +1,23 @@
 package main;
 
 import client.InfinimumDBClient;
+import de.hhu.bsinfo.infinileap.binding.ControlException;
+import de.hhu.bsinfo.infinileap.util.CloseException;
+import exceptions.NotFoundException;
+import org.apache.commons.lang3.SerializationUtils;
 
 public final class Application {
     public static void main(final String... args) {
         final InfinimumDBClient client = new InfinimumDBClient("localhost", 2998, "localhost", 6379);
-        client.put("I am the key2", "I am the value");
+        client.put("I am the key", "I am the value");
+        byte[] object = new byte[0];
+        try {
+            object = client.get("I do not exist");
+        } catch (CloseException | NotFoundException | ControlException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (object.length > 1) {
+            System.out.println(SerializationUtils.deserialize(object).toString());
+        }
     }
 }
