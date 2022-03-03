5334dd1aa5b2ca0254e13223c480a08d20f61a14
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-03-03 13:55:49 +0100

    ✅ add tests for hash collision handling

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -19,10 +19,40 @@ public final class Application {
     private static final Integer serverPort = 2998;
 
     public static void main(final String... args) throws CloseException, NotFoundException, NoSuchAlgorithmException, ControlException, InterruptedException, DuplicateKeyException {
-        //testDelete();
         testCanPutAndGetObject();
         testTwoKeyValuesWork();
-        testTwoKeysWithCollidingHashCanBePut();
+        testTwoKeysWithCollidingHash();
+        testThreeKeysWithCollidingHash();
+        testThreeKeysWithCollidingHashDeletingInOrder1();
+        testThreeKeysWithCollidingHashDeletingInOrder2();
+        testThreeKeysWithCollidingHashDeletingInOrder3();
+    }
+
+    private static void testDelete() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, DuplicateKeyException {
+        log.debug("Start testCanPutAndGetObject:");
+        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
+        String key = "This is a key";
+        String key2 = "This is a key2";
+        String key3 = "hash_collision_test_1";
+        String key4 = "hash_collision_test_2";
+
+        try {
+            client.del(key);
+        } catch (NotFoundException e) {
+        }
+        try {
+            client.del(key2);
+        } catch (NotFoundException e) {
+        }
+        try {
+            client.del(key3);
+        } catch (NotFoundException e) {
+        }
+        try {
+            client.del(key4);
+        } catch (NotFoundException e) {
+        }
+        log.debug("End testCanPutAndGetObject:");
     }
 
     private static void testCanPutAndGetObject() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
@@ -36,14 +66,6 @@ public final class Application {
 
         assertArrayEquals(value, response);
 
-        client.del(key);
-        log.debug("End testCanPutAndGetObject:");
-    }
-
-    private static void testDelete() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
-        log.debug("Start testCanPutAndGetObject:");
-        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
-        String key = "This is a key";
         client.del(key);
         log.debug("End testCanPutAndGetObject:");
     }
@@ -69,18 +91,12 @@ public final class Application {
         log.debug("End testTwoKeyValuesWork:");
     }
 
-    private static void testTwoKeysWithCollidingHashCanBePut() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
-        log.debug("Start testTwoKeysWithCollidingHashCanBePut:");
+    private static void testTwoKeysWithCollidingHash() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
+        log.debug("Start testTwoKeysWithCollidingHash:");
         InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
-        String key = "d131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f89" +
-                "55ad340609f4b30283e488832571415a085125e8f7cdc99fd91dbdf280373c5b" +
-                "d8823e3156348f5bae6dacd436c919c6dd53e2b487da03fd02396306d248cda0" +
-                "e99f33420f577ee8ce54b67080a80d1ec69821bcb6a8839396f9652b6ff72a70";
+        String key = "hash_collision_test_1";
         byte[] value = SerializationUtils.serialize("This is a value");
-        String key2 = "d131dd02c5e6eec4693d9a0698aff95c2fcab50712467eab4004583eb8fb7f89" +
-                "55ad340609f4b30283e4888325f1415a085125e8f7cdc99fd91dbd7280373c5b" +
-                "d8823e3156348f5bae6dacd436c919c6dd53e23487da03fd02396306d248cda0" +
-                "e99f33420f577ee8ce54b67080280d1ec69821bcb6a8839396f965ab6ff72a70";
+        String key2 = "hash_collision_test_2";
         byte[] value2 = SerializationUtils.serialize("This is a value2");
 
         client.put(key, value);
@@ -94,5 +110,122 @@ public final class Application {
         client.del(key);
         client.del(key2);
         log.debug("End testTwoKeysWithCollidingHashCanBePut:");
+    }
+
+    private static void testThreeKeysWithCollidingHash() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
+        log.debug("Start testThreeKeysWithCollidingHash:");
+        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
+        String key = "hash_collision_test_1";
+        byte[] value = SerializationUtils.serialize("This is a value");
+        String key2 = "hash_collision_test_2";
+        byte[] value2 = SerializationUtils.serialize("This is a value2");
+        String key3 = "hash_collision_test_3";
+        byte[] value3 = SerializationUtils.serialize("This is a value3");
+
+        client.put(key, value);
+        client.put(key2, value2);
+        client.put(key3, value3);
+        byte[] response = client.get(key);
+        byte[] response2 = client.get(key2);
+        byte[] response3 = client.get(key3);
+
+        assertArrayEquals(value, response);
+        assertArrayEquals(value2, response2);
+        assertArrayEquals(value3, response3);
+
+        client.del(key);
+        client.del(key2);
+        client.del(key3);
+        log.debug("End testTwoKeysWithCollidingHashCanBePut:");
+    }
+
+    private static void testThreeKeysWithCollidingHashDeletingInOrder1() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
+        log.debug("Start testThreeKeysWithCollidingHash:");
+        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
+        String key = "hash_collision_test_1";
+        byte[] value = SerializationUtils.serialize("This is a value");
+        String key2 = "hash_collision_test_2";
+        byte[] value2 = SerializationUtils.serialize("This is a value2");
+        String key3 = "hash_collision_test_3";
+        byte[] value3 = SerializationUtils.serialize("This is a value3");
+
+        client.put(key, value);
+        client.put(key2, value2);
+        client.put(key3, value3);
+
+        client.del(key);
+
+        byte[] response2 = client.get(key2);
+        byte[] response3 = client.get(key3);
+        assertArrayEquals(value2, response2);
+        assertArrayEquals(value3, response3);
+
+        client.del(key2);
+
+        response3 = client.get(key3);
+        assertArrayEquals(value3, response3);
+
+        client.del(key3);
+        log.debug("End testTwoKeysWithCollidingHashCanBePut:");
+    }
+
+    private static void testThreeKeysWithCollidingHashDeletingInOrder2() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
+        log.debug("Start testThreeKeysWithCollidingHashDeletingInOrder2:");
+        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
+        String key = "hash_collision_test_1";
+        byte[] value = SerializationUtils.serialize("This is a value");
+        String key2 = "hash_collision_test_2";
+        byte[] value2 = SerializationUtils.serialize("This is a value2");
+        String key3 = "hash_collision_test_3";
+        byte[] value3 = SerializationUtils.serialize("This is a value3");
+
+        client.put(key, value);
+        client.put(key2, value2);
+        client.put(key3, value3);
+
+        client.del(key3);
+
+        byte[] response = client.get(key);
+        byte[] response2 = client.get(key2);
+        assertArrayEquals(value, response);
+        assertArrayEquals(value2, response2);
+
+        client.del(key2);
+
+        response = client.get(key);
+        assertArrayEquals(value, response);
+
+        client.del(key);
+        log.debug("End testThreeKeysWithCollidingHashDeletingInOrder2:");
+    }
+
+    private static void testThreeKeysWithCollidingHashDeletingInOrder3() throws CloseException, NoSuchAlgorithmException, ControlException, InterruptedException, NotFoundException, DuplicateKeyException {
+        log.debug("Start testThreeKeysWithCollidingHashDeletingInOrder3:");
+        InfinimumDBClient client = new InfinimumDBClient(serverHostAddress, serverPort);
+        String key = "hash_collision_test_1";
+        byte[] value = SerializationUtils.serialize("This is a value");
+        String key2 = "hash_collision_test_2";
+        byte[] value2 = SerializationUtils.serialize("This is a value2");
+        String key3 = "hash_collision_test_3";
+        byte[] value3 = SerializationUtils.serialize("This is a value3");
+
+        client.put(key, value);
+        client.put(key2, value2);
+        client.put(key3, value3);
+
+        client.del(key2);
+
+        byte[] response = client.get(key);
+        byte[] response3 = client.get(key3);
+        assertArrayEquals(value, response);
+        assertArrayEquals(value3, response3);
+
+        client.del(key3);
+
+        response = client.get(key);
+        assertArrayEquals(value, response);
+
+        client.del(key);
+        log.debug("End testThreeKeysWithCollidingHashDeletingInOrder3:");
     }
 }
