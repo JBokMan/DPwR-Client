a243988fdf4f461816d7cc3056cdb4f92759adb7
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-03-03 13:55:06 +0100

    :sparkles: add test mode to md5 hashing

--- src/main/java/client/CommunicationUtils.java
+++ src/main/java/client/CommunicationUtils.java
@@ -13,14 +13,28 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class CommunicationUtils {
 
+    private static final boolean TEST_MODE = true;
+
     public static byte[] getMD5Hash(final String text) throws NoSuchAlgorithmException {
         final MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        return messageDigest.digest(text.getBytes(StandardCharsets.UTF_8));
+        byte[] id = messageDigest.digest(text.getBytes(StandardCharsets.UTF_8));
+        if (TEST_MODE) {
+            if (text.contains("hash_collision_test")) {
+                id = new byte[16];
+            }
+        }
+        // If all bits are zero there are problems with the next entry id's of the plasma entry's
+        if (Arrays.equals(id, new byte[16])) {
+            // Set the first bit to 1
+            id[0] |= 1 << (0);
+        }
+        return id;
     }
 
     private static String bytesToHex(final byte[] bytes) {
