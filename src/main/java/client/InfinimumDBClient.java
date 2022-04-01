61ec50b7295a820a8ad876bd0a32e571146df36f
Julian Bokelmann <J.B.Bokelmann@gmail.com>
2022-04-01 19:04:38 +0200

    :sparkles: reuse worker for operations

--- src/main/java/client/InfinimumDBClient.java
+++ src/main/java/client/InfinimumDBClient.java
@@ -23,15 +23,32 @@ import static utils.CommunicationUtils.*;
 
 @Slf4j
 public class InfinimumDBClient {
-    private transient final ResourcePool resources = new ResourcePool();
-    private transient final Map<Integer, InetSocketAddress> serverMap = new HashMap<>();
-    private transient Worker worker;
-    private transient Endpoint endpoint;
+    private final ResourcePool resources = new ResourcePool();
+    private final Map<Integer, InetSocketAddress> serverMap = new HashMap<>();
+    private Context context;
+    private Worker worker;
 
     private static final ContextParameters.Feature[] FEATURE_SET = {ContextParameters.Feature.TAG, ContextParameters.Feature.RMA};
 
     public InfinimumDBClient(final String serverHostAddress, final Integer serverPort) {
         this.serverMap.put(0, new InetSocketAddress(serverHostAddress, serverPort));
+        NativeLogger.enable();
+        log.info("Using UCX version {}", Context.getVersion());
+
+        try {
+            // Initialize UCP context
+            log.info("Initializing context");
+            final ContextParameters contextParameters = new ContextParameters().setFeatures(FEATURE_SET);
+            this.context = Context.initialize(contextParameters, null);
+
+            // Create a worker
+            log.info("Creating worker");
+            final WorkerParameters workerParameters = new WorkerParameters().setThreadMode(ThreadMode.SINGLE);
+            this.worker = context.createWorker(workerParameters);
+        } catch (final ControlException e) {
+            e.printStackTrace();
+        }
+
         setupServerConnection(serverHostAddress, serverPort);
         try {
             testServerConnection();
@@ -50,41 +67,44 @@ public class InfinimumDBClient {
 
     public void put(final String key, final byte[] value, final int timeoutMs, final int maxAttempts) throws CloseException, ControlException, DuplicateKeyException, TimeoutException {
         try (resources) {
-            initialize(key);
-            putOperation(key, value, timeoutMs);
+            final Endpoint endpoint = createEndpoint(key);
+            putOperation(key, value, timeoutMs, endpoint);
         } catch (final CloseException | ControlException | TimeoutException e) {
             if (maxAttempts == 1) {
                 throw e;
             }
+            resetWorker();
             put(key, value, timeoutMs, maxAttempts - 1);
         }
     }
 
     public byte[] get(final String key, final int timeoutMs, final int maxAttempts) throws CloseException, NotFoundException, ControlException, TimeoutException, SerializationException {
         try (resources) {
-            initialize(key);
-            return getOperation(key, timeoutMs);
+            final Endpoint endpoint = createEndpoint(key);
+            return getOperation(key, timeoutMs, endpoint);
         } catch (final CloseException | ControlException | TimeoutException | SerializationException e) {
             if (maxAttempts == 1) {
                 throw e;
             }
+            resetWorker();
             return get(key, timeoutMs, maxAttempts - 1);
         }
     }
 
     public void del(final String key, final int timeoutMs, final int maxAttempts) throws CloseException, NotFoundException, ControlException, TimeoutException {
         try (resources) {
-            initialize(key);
-            delOperation(key, timeoutMs);
+            final Endpoint endpoint = createEndpoint(key);
+            delOperation(key, timeoutMs, endpoint);
         } catch (final CloseException | ControlException | TimeoutException e) {
             if (maxAttempts == 1) {
                 throw e;
             }
+            resetWorker();
             del(key, timeoutMs, maxAttempts - 1);
         }
     }
 
-    public void putOperation(final String key, final byte[] value, final int timeoutMs) throws SerializationException, ControlException, DuplicateKeyException, TimeoutException {
+    public void putOperation(final String key, final byte[] value, final int timeoutMs, final Endpoint endpoint) throws SerializationException, ControlException, DuplicateKeyException, TimeoutException {
         log.info("Starting PUT operation");
         final int tagID = receiveTagID(worker, timeoutMs);
         final byte[] entryBytes = serialize(new PlasmaEntry(key, value, new byte[20]));
@@ -112,7 +132,7 @@ public class InfinimumDBClient {
     }
 
 
-    private byte[] getOperation(final String key, final int timeoutMs) throws ControlException, NotFoundException, TimeoutException, SerializationException {
+    private byte[] getOperation(final String key, final int timeoutMs, final Endpoint endpoint) throws ControlException, NotFoundException, TimeoutException, SerializationException {
         log.info("Starting GET operation");
         final int tagID = receiveTagID(worker, timeoutMs);
         try (final ResourceScope scope = ResourceScope.newConfinedScope()) {
@@ -138,7 +158,7 @@ public class InfinimumDBClient {
         return value;
     }
 
-    private void delOperation(final String key, final int timeoutMs) throws NotFoundException, TimeoutException {
+    private void delOperation(final String key, final int timeoutMs, final Endpoint endpoint) throws NotFoundException, TimeoutException {
         log.info("Starting DEL operation");
         final int tagID = receiveTagID(worker, timeoutMs);
         final ArrayList<Long> requests = new ArrayList<>();
@@ -159,31 +179,28 @@ public class InfinimumDBClient {
         log.info("Del completed\n");
     }
 
-    private void initialize(final String key) throws ControlException {
-        NativeLogger.enable();
-        log.info("Using UCX version {}", Context.getVersion());
-
-        // Initialize UCP context
-        log.info("Initializing context");
-        final ContextParameters contextParameters = new ContextParameters().setFeatures(FEATURE_SET);
-        final Context context = pushResource(Context.initialize(contextParameters, null));
-
-        // Create a worker
-        log.info("Creating worker");
-        final WorkerParameters workerParameters = new WorkerParameters().setThreadMode(ThreadMode.SINGLE);
-        this.worker = pushResource(context.createWorker(workerParameters));
-
-        // Determining responsible server
-        final Integer responsibleServerID = getResponsibleServerID(key, this.serverMap.size());
-        final EndpointParameters endpointParams = new EndpointParameters().setRemoteAddress(this.serverMap.get(responsibleServerID)).setPeerErrorHandlingMode();
-
-        // Creating Endpoint
-        log.info("Connecting to {}", this.serverMap.get(responsibleServerID));
-        this.endpoint = pushResource(worker.createEndpoint(endpointParams));
-    }
-
     protected <T extends AutoCloseable> T pushResource(final T resource) {
         resources.push(resource);
         return resource;
+    }
+
+    private Endpoint createEndpoint(final String key) throws ControlException {
+        // Determining responsible server
+        final Integer responsibleServerID = getResponsibleServerID(key, this.serverMap.size());
+
+        // Creating Endpoint
+        log.info("Connecting to {}", this.serverMap.get(responsibleServerID));
+        final EndpointParameters endpointParams = new EndpointParameters().setRemoteAddress(this.serverMap.get(responsibleServerID)).setPeerErrorHandlingMode();
+        return pushResource(this.worker.createEndpoint(endpointParams));
+    }
+
+    private void resetWorker() {
+        this.worker.close();
+        final WorkerParameters workerParameters = new WorkerParameters().setThreadMode(ThreadMode.SINGLE);
+        try {
+            this.worker = context.createWorker(workerParameters);
+        } catch (final ControlException e) {
+            e.printStackTrace();
+        }
     }
 }
